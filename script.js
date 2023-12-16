ff7e3c94fdf1bf61bf4619ef465cb95edbee205f
mohan_chandra <129154398+Ravisupreme-15@users.noreply.github.com>
2023-12-16 11:17:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function myFunction() {
+	alert("First Name:" document.getElementById("fname").value);
+	// alert("Lirst Name:" document.getElementById("lname").value);
+	// alert("Phone Number:" document.getElementById("pno").value);
+	// alert("Email ID:" document.getElementById("emailid").value);
+}
